b42b56d3b924d02a441ede3d71c0b158a8ed40e0
Nick Faber <nfaber@sandiego.edu>
2021-03-12 22:21:55 -0800

    final commit

--- Code/Solved/plotsnf.js
+++ Code/Solved/plotsnf.js
@@ -20,9 +20,59 @@ Plotly.d3.csv('../../Hotels/CSV/finalNetIncome.csv', function (rows) {
         yaxis: 'y'
     };
     console.log(trace);
-//   });
 
-  // Initializes the page with a default plot
+    var trace1 = {
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'Hilton'),
+  
+        // specify type of chart and axes
+        name: 'Final Net Income',
+        type: 'line',
+        xaxis: 'x',
+        yaxis: 'y'
+    };
+    console.log(trace1);
+
+    var trace2 = {
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'Hyatt'),
+  
+        // specify type of chart and axes
+        name: 'Final Net Income',
+        type: 'line',
+        xaxis: 'x',
+        yaxis: 'y'
+    };
+    console.log(trace2);
+
+    var trace3 = {
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'Marriott'),
+  
+        // specify type of chart and axes
+        name: 'Final Net Income',
+        type: 'line',
+        xaxis: 'x',
+        yaxis: 'y'
+    };
+    console.log(trace3);
+
+    var trace4 = {
+        x: unpack(rows, 'date'),
+        y: unpack(rows, 'Wyndham'),
+  
+        // specify type of chart and axes
+        name: 'Final Net Income',
+        type: 'line',
+        xaxis: 'x',
+        yaxis: 'y'
+    };
+    console.log(trace4);
+  });
+
+  var data = [trace, trace1, trace2, trace3, trace4];
+
+//   // Initializes the page with a default plot
     function init() {
         var dataset1 = [trace];
   
@@ -30,50 +80,28 @@ Plotly.d3.csv('../../Hotels/CSV/finalNetIncome.csv', function (rows) {
   
         Plotly.newPlot(CHART, dataset1);
         console.log(dataset1)
-  };
+    };
 // });
   // Call updatePlotly() when a change takes place to the DOM
-    d3.selectAll("body").on("change", updatePlotly);
+    d3.selectAll("#selDataset").on("change", updatePlotly);
   
-//   This function is called when a dropdown menu item is selected
+// //   This function is called when a dropdown menu item is selected
     function updatePlotly() {
-        // Use D3 to select the dropdown menu
+//         // Use D3 to select the dropdown menu
         var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
+//     // Assign the value of the dropdown menu option to a variable
         var dataset = dropdownMenu.node().value;
   
         var CHART = d3.selectAll("#plot").node();
-  
-    // Initialize x and y arrays
-        var x = [];
-        var y = [];
+
+    };
   
         switch(trace) {
         case "dataset1":
-            x = [1, 2, 3, 4, 5];
-            y = [1, 2, 4, 8, 16];
+            x = rows.map.BestWestern;
+            y = rows.map.date;
             break;
-  
-        case "dataset2":
-            x = [10, 20, 30, 40, 50];
-            y = [1, 10, 100, 1000, 10000];
-            break;
-  
-        case "dataset3":
-            x = [100, 200, 300, 400, 500];
-            y = [10, 100, 50, 10, 0];
-            break;
-    
-        default:
-            x = [1, 2, 3, 4, 5];
-            y = [1, 2, 3, 4, 5];
-            break;
-        }
-    
-    
-        // Note the extra brackets around 'x' and 'y'
-        Plotly.restyle(CHART, "x", [x]);
-        Plotly.restyle(CHART, "y", [y]);
-    }
-});
+
+        };
   init();
+
